5f5e917f29bb360a1a8d79aaa1ace5a8b761eaa4
JakubSapko <jakub.sapko1@gmail.com>
2023-03-15 19:31:10 +0100

    add: figure out match statements

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let command: &String = &args[1];
+
+    match command.as_str() {
+        "help" => println!("This will print help"),
+        "config" => println!("This will setup config"),
+        _ => println!("This will print error message"),
+    }
 }
