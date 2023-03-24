9e0c7433b58412f9bd7002b88d61cee3065cceb2
JakubSapko <jakub.sapko1@gmail.com>
2023-03-24 08:57:37 +0100

    add: user can now add his openai key

--- src/api/config/config.rs
+++ src/api/config/config.rs
@@ -1,16 +1,27 @@
-use std::env;
-use env_perm::{check_or_set};
+use confy::store;
+use serde_derive::{Deserialize, Serialize};
+use std;
 
-pub fn set_api_key(api_key: &Option<std::string::String>)  {
-    
+#[derive(Serialize, Deserialize)]
+struct ZiomConfig {
+    api_key: String,
+}
+
+pub fn set_api_key(api_key: &Option<std::string::String>) {
     match api_key {
         Some(value) => {
             let key = "ZIOM-OPEN-API";
-            check_or_set(key, value);
-            println!("Key set successfully!");
-        },
+            let ziom_cfg = ZiomConfig {
+                api_key: key.to_string(),
+            };
+            let store = store("ziom", None, ziom_cfg);
+            match store {
+                Ok(_) => println!("Key properly set!"),
+                Err(e) => println!("Error occured: {e:?}"),
+            }
+        }
         None => {
             println!("Please provide your OpenAI API key!")
-        },
+        }
     }
 }
