cce3c1095b4a1209187f0cac7da8a55291cbb9f7
JakubSapko <jakub.sapko1@gmail.com>
2023-03-25 10:38:58 +0100

    add: user can now read his set api key

--- src/api/config/config.rs
+++ src/api/config/config.rs
@@ -1,8 +1,8 @@
-use confy::store;
-use serde_derive::{Deserialize, Serialize};
-use std;
 
-#[derive(Serialize, Deserialize)]
+use confy::{load, store, ConfyError};
+use serde_derive::{Deserialize, Serialize};
+
+#[derive(Default, Serialize, Deserialize)]
 struct ZiomConfig {
     api_key: String,
 }
@@ -16,11 +16,20 @@ pub fn set_api_key(api_key: &Option<std::string::String>) {
             let store = store("ziom", None, ziom_cfg);
             match store {
                 Ok(_) => println!("Key properly set!"),
-                Err(e) => println!("Error occured: {e:?}"),
+                Err(e) => println!("Error occured: {:?}", e),
             }
         }
         None => {
-            println!("Please provide your OpenAI API key!")
+            let key = read_api_key();
+            match key {
+                Ok(value) => println!("Your API key: {}", value),
+                Err(e) => println!("Error occured: {:?}", e),
+            }
         }
     }
+}
+
+pub fn read_api_key() -> Result<String, ConfyError> {
+    let ziom_cfg = confy::load::<ZiomConfig>("ziom", None)?;
+    return Ok(ziom_cfg.api_key);
 }
