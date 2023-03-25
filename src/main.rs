6864a0ce7d8f14d2ceb1de20148b557a85982bfd
JakubSapko <jakub.sapko1@gmail.com>
2023-03-25 10:58:13 +0100

    clean up dead code

--- src/main.rs
+++ src/main.rs
@@ -30,15 +30,6 @@ struct Generate {
 }
 
 fn main() {
-    // let args: Vec<String> = env::args().collect();
-    // let command: &String = &args[1];
-
-    // match command.as_str() {
-    //     "help" => println!("This will print help"),
-    //     "config" => println!("This will setup config"),
-    //     _ => println!("This will print error message"),
-    // }
-
     let cli = Cli::parse();
 
     match &cli.command {
